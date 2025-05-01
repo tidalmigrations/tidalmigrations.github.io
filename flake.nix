56c21e8565d769317fc3aa3edcc95c78112cac57
Justin Barclay <git@justinbarclay.ca>
2025-05-01 13:18:42 -0700

    chore: add flake and update

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Jekyll blog development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.ruby_3_4
+
+            ];
+            shellHook = ''
+              export PKG_CONFIG_PATH=$PKG_CONFIG_PATH_FOR_TARGET;
+              ruby -v
+            '';
+          };
+        };
+      });
+}
